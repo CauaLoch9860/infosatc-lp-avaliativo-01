a22b03774929ebcc8d383e81f96e1108c3c8b0ca
CauaLoch9860 <88676881+CauaLoch9860@users.noreply.github.com>
2021-08-30 14:36:01 -0300

    Create ex4

---
+++ ex4.py
@@ -0,0 +1,16 @@
+
+premio = float(input("Digite o Valor Total do Premio: "))
+premiodesc = premio - premio * 0.07
+imposto = premio * 0.07
+
+ganhador1 = (premiodesc * 0.46)
+ganhador2 = (premiodesc * 0.32)
+ganhador3 = premiodesc - (ganhador1 + ganhador2)
+
+print("\nValor Premio Total: ",premio)
+print("\nValor Premio Descontado: ",premiodesc)
+print("\nValor do imposto Descontado: ",imposto)
+print("\n============Ganhadores=====================\n")
+print("\nValor Premio primeiro ganhador: ",ganhador1)
+print("\nValor Premio segundo  ganhador: ",ganhador2)
+print("\nValor Premio terceiro ganhador: ",ganhador3)
