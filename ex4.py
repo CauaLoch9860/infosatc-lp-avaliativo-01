4048dd3cd422f404be389a8df00d0c760b55aea3
CauaLoch9860 <88676881+CauaLoch9860@users.noreply.github.com>
2021-08-30 14:51:21 -0300

    Comentando

--- ex4.py
+++ ex4.py
@@ -1,12 +1,12 @@
-
+#Váriaveis e cálculo
 premio = float(input("Digite o Valor Total do Premio: "))
 premiodesc = premio - premio * 0.07
 imposto = premio * 0.07
-
+#cálculo dos ganhadores
 ganhador1 = (premiodesc * 0.46)
 ganhador2 = (premiodesc * 0.32)
 ganhador3 = premiodesc - (ganhador1 + ganhador2)
-
+#Mostrar na tela as informações
 print("\nValor Premio Total: ",premio)
 print("\nValor Premio Descontado: ",premiodesc)
 print("\nValor do imposto Descontado: ",imposto)
