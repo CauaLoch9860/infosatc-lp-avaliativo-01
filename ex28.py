c316cea472e83139bcb34ca2835d794928be57c4
CauaLoch9860 <88676881+CauaLoch9860@users.noreply.github.com>
2021-08-30 15:13:07 -0300

    ex28

---
+++ ex28.py
@@ -0,0 +1,8 @@
+#Cálculo e Váriaveis
+n1 = float(input("digite o valor 1: "))
+n2 = float(input("digite o valor 2:"))
+n3 = float(input("digite o valor 3: "))
+
+soma= (n1+n2+n3)
+#Mostrar a tela 
+print("A soma dos valores ficou:",soma)
