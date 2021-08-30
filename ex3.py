88a09a542834d12a3422b4beaae6da12de7e6793
CauaLoch9860 <88676881+CauaLoch9860@users.noreply.github.com>
2021-08-30 14:49:46 -0300

    Comentando

--- ex3.py
+++ ex3.py
@@ -1,11 +1,12 @@
 import math
-
-altura = float(input("digite a altura: "))
+#Variaveis 
+altura = float(input("digite a altura: ")) 
 comprimento = float(input("digite o comprimento: "))
+#cálculo
 medida = (altura*comprimento)
 litros = (medida / 3)
 latas = math.ceil(litros / 3.6)
 preco = (latas* 107)
-
+#Mostrar na tela 
 print("Valor:",preco)
 print("Quantidades de lata:",latas)
