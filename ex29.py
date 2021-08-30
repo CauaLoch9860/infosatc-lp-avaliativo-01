24e24f542a530259aba59d6e9d7fb4229071abd2
CauaLoch9860 <88676881+CauaLoch9860@users.noreply.github.com>
2021-08-30 15:15:50 -0300

    ex29

---
+++ ex29.py
@@ -0,0 +1,8 @@
+n1 = float(input("digite a nota 1: "))
+n2 = float(input("digite a nota 2:"))
+n3 = float(input("digite a nota 3: "))
+n4 = float(input("digite a nota 4: "))
+
+media= (n1+n2+n3+n4)/4
+#Mostrar a tela 
+print("A a media das notas ficou:",media)
